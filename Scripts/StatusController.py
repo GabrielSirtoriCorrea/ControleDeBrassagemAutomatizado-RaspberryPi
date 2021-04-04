75a1f28fb1523a3dc79b3b8e5bc9187bd0fc2490
Gazebo <gabrielsirtori.correa@gmail.com>
2021-04-04 16:14:08 -0300

    ResetStatusEmBrewController

--- Scripts/StatusController.py
+++ Scripts/StatusController.py
@@ -15,3 +15,8 @@ def writeStatus(tank, target, value):
     file = open(path, 'w')
     json.dump(status, file)
 
+def resetStatus():
+    defaultStatus = {'BrewStatus':'Pausado','Tank3':{'Temperature':0,'NextProcess':False,'Hops':[0,0,0],'BoilTime':0,'SetPoint':0,'Resistence':False,'HopAlert':False},'Tank2':{'ClarificationTime':0,'Motor':False,'NextProcess':False},'BrewMode':'Automatic','Tank1':{'Ramps':[[0,0],[0,0]],'MaltAlert':False,'Temperature':0,'Motor':False,'NextProcess':False,'SetPoint':0,'ActualRamp':0,'Resistence':False},'Bomb':False}
+    file = open(path, 'w')
+    json.dump(defaultStatus, file)
+
